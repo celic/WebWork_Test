7999f9e3e0998474e3e844ad68d974a88620acf2
celic <cceli@codequarry.net>
2013-10-01 12:51:07 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebworkTest::Application.config.secret_token = '2184896e8e11c6be5362b3628851b43e798dc45d085d025874866d358314efa612e4cb280d3339006c145b89d46203b0177b64941016ec067c9263eb03e142a0'
